05032f2b1125ddb9769464433c443f5f9d98eab1
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2023-04-02 19:46:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+
+
+
+let `a` = 10;
+alert (a);
+
+`a` = 20;
+alert (a);
+
+2.
+let firstIphoneYear = 2007;
+alert(firstIphoneYear);
+
+3.
+let `userName` = 'Brandan Eich';
+alert (userName);
+
+let `userName` = 'Brandan Eich';
+console.log (userName);
+
+4.
+let `a` = 10;
+let `b` = 2;
+alert (a + b);
+alert (a - b);
+alert (a * b);
+alert (a / b);
+
+5.
+let result = (2 ** 5);
+alert (result);
+
+6.
+let a = 9;
+let b = 2;
+let remainder = (9 % 2);
+alert (remainder);
+
+7.
+let num = 1;
+num += 5;
+num -=3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+8.
+let age = Number (prompt (`Сколько вам лет?`));
+alert (age);
+
+9.
+const {};
+const user = {
+    name: `Kristina`,
+    age: 33,
+    isAdmin: true
+};
+console.log (user);
+
+9.1
+const user = {
+    name: `Kristina`,
+    age: 33,
+    isAdmin: true,
+};
+user [`city of residence`] = `Kuala Lumpur`;
+console.log (user);
+
+9.2
+const user = {
+    name: `Kristina`,
+    age: 33,
+    isAdmin: true,
+};
+user [`city of residence`] = `Kuala Lumpur`;
+user.age = 34;
+console.log (user);
+
+9.3
+const user = {
+    name: `Kristina`,
+    age: 33,
+    isAdmin: true,
+};
+user [`city of residence`] = `Kuala Lumpur`;
+user.age = 34;
+delete user[`city of residence`];
+console.log (user);
+
+9.4
+let info = prompt(`Какую информацию хотите узнать о пользователе?`);
+const user = {
+    name: `Kristina`,
+    age: 33,
+    }
+    alert(user[info]);
+
+    10.
+    let `userName` = prompt (`Как тебя зовут?`);
+    alert(`Привет, ${userName}!`);
+ 

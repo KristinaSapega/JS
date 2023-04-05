2060873402bb3f00e03563221c85ebf2b55f727e
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2023-04-05 16:42:44 +0800

    2

--- script3.js
+++ script3.js
@@ -1,13 +1,14 @@
 
-//1.
-const password = `Qwerty12345`;
-const userInput = prompt (`Введите пароль`);
-if (userInput === password ) {
-    console.log (`Пароль введен верно`);
-}
-else {
-   console.log (`Пароль введен неправильно`);
+//2.
+let c = 2;
+if (c > 0 && c < 10) {
+    console.log(`Верно`);
+} else {
+    console.log(`Неверно`);
 }
 
-
- 
+// Результаты для значений c:
+// c = 0: Неверно
+// c = 10: Неверно
+// c = -3: Неверно
+// c = 2: Верно

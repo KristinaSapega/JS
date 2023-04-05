4457d254ea0014de99f7367eb6e5f47bb36077ac
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2023-04-05 16:43:10 +0800

    2.2

--- script3.js
+++ script3.js
@@ -1,14 +1,9 @@
 
-//2.
-let c = 2;
-if (c > 0 && c < 10) {
-    console.log(`Верно`);
+//2.2
+let c = 7;
+let userInput = prompt (`Введите число`);
+if (userInput == c && c > 0 && c < 10) {
+    console.log (`Верно`);   
 } else {
-    console.log(`Неверно`);
+    console.log (`Неверно`);
 }
-
-// Результаты для значений c:
-// c = 0: Неверно
-// c = 10: Неверно
-// c = -3: Неверно
-// c = 2: Верно

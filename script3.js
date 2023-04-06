eeab66d97d4e6db266f6930b93b52f43691d920c
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2023-04-06 23:50:21 +0800

    Fixed tasks 4, 5

--- script3.js
+++ script3.js
@@ -7,3 +7,33 @@ alert(a + b);
 let a = `2`;
 let b = `3`;
 alert (Number (a) + Number (b));
+
+//5.
+let monthNumber = Number( prompt (`Введите номер месяца`) );
+if (monthNumber <= 12) {
+switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+    console.log (`Зима`);
+    break;
+    case 3:
+    case 4:
+    case 5:
+    console.log (`Весна`);
+    break;
+    case 6:
+    case 7:
+    case 8:
+    console.log (`Лето`);
+    break;
+    case 9:
+    case 10:
+    case 11:
+    console.log (`Осень`);
+    break;
+    default:
+    console.log(`Такого месяца не существует`);
+    break;
+}
+}  
